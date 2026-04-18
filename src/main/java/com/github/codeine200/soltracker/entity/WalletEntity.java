5fed4c2ace90159f9fdcf313bf6ae6640e43a5b4
v.holovko <codeine200@gmail.com>
2026-04-18 23:59:01 +0300

    add db

---
+++ src/main/java/com/github/codeine200/soltracker/entity/WalletEntity.java
@@ -0,0 +1,28 @@
+package com.github.codeine200.soltracker.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "wallet")
+public class WalletEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String address;
+
+    @Lob
+    @Column(name = "private_key")
+    private byte[] privateKey;
+
+    private String prefix;
+
+    private String suffix;
+}
---
+++ src/main/resources/V1__create_wallet.sql
@@ -0,0 +1,11 @@
+CREATE TABLE wallet (
+   id BIGSERIAL PRIMARY KEY,
+   address VARCHAR(44) NOT NULL,
+   private_key BYTEA NOT NULL,
+   prefix VARCHAR(3) NOT NULL,
+   suffix VARCHAR(3) NOT NULL,
+
+   CONSTRAINT uq_wallet_prefix_suffix UNIQUE (prefix, suffix)
+);
+
+CREATE INDEX idx_wallet_prefix_suffix ON wallet(prefix, suffix);
